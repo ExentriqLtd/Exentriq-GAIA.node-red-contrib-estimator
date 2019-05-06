b39a5fc4ee9e2d09b40fd6a53a5e46530de04e55
Emilio Sabatucci <sabatucci@gmail.com>
2019-05-06 13:52:39 +0200

    fixes for node red

--- estimator.js
+++ estimator.js
@@ -5,18 +5,7 @@
         var node = this;
 
         try {
-            this.on('input', function(msg) {
-        	
-        		//layout preparation
-    			var items = msg.payload.items;
-        		
-                var tmpLayouts = runPacker(items, itemsArtMap, node);
-                
-                msg.payload.layouts = tmpLayouts;
-                
-                node.send(msg);
-            });
-        } catch (e) {
+                    } catch (e) {
 	        node.error("ops, there was an error!", msg);
         }
     }
